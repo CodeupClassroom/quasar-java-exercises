07c4a2f2e7a24560a8c7b53dbeaa450fe06fcdea
Larry Castillo <larrycastillo2009@gmail.com>
2021-10-27 10:36:52 -0500

    exercise setup

---
+++ src/Highlow.java
@@ -0,0 +1,21 @@
+public class Highlow {
+
+    public static void main(String[] args) {
+
+    }
+
+    //    5. Game Development 101
+
+//    Welcome to the world of game development!
+//
+//    You are going to build a high-low guessing game. Create a class named HighLow inside of src.
+//
+//    The specs for the game are:
+//
+//    - Game picks a random number between 1 and 100.
+//    - Prompts user to guess the number.
+//    - All user inputs are validated.
+//    - If user's guess is less than the number, it outputs "HIGHER".
+//    - If user's guess is more than the number, it outputs "LOWER".
+//    - If a user guesses the number, the game should declare "GOOD GUESS!"
+}
---
+++ src/MethodsExercise.java
@@ -0,0 +1,73 @@
+public class MethodsExercise {
+    public static void main(String[] args) {
+
+    }
+
+//    1.Basic Arithmetic
+//
+//    a. Create four separate methods. Each will perform an arithmetic operation:
+//
+//    -Addition
+//    -Subtraction
+//    -Multiplication
+//    -Division
+//
+//    b. Each function needs to take two parameters/arguments so that the operation can be performed.
+//    c. Test your methods and verify the output.
+//    d. Add a modulus method that finds the modulus of two numbers.
+//
+//    Food for thought: What happens if we try to divide by zero? What should happen?
+
+
+
+
+
+//    2. Create a method that validates that user input is in a certain range
+//
+//    The method signature should look like this:
+//
+//      public static int getInteger(int min, int max);
+//
+//    and is used like this:
+//
+//      System.out.print("Enter a number between 1 and 10: ");
+//      int userInput = getInteger(1, 10);
+
+//    If the input is invalid, prompt the user again.
+
+
+
+
+//    3. Calculate the factorial of a number.
+//
+//    - Prompt the user to enter an integer from 1 to 10.
+//    - Display the factorial of the number entered by the user.
+//    - Ask if the user wants to continue.
+//    - Use a for loop to calculate the factorial.
+//    - Assume that the user will enter an integer, but verify it’s between 1 and 10.
+//    - Use the long type to store the factorial.
+//    - Continue only if the user agrees to.
+//    - A factorial is a number multiplied by each of the numbers before it.
+//    - Factorials are denoted by the exclamation point (n!). Ex:
+
+//            1! = 1               = 1
+//            2! = 1 x 2           = 2
+//            3! = 1 x 2 x 3       = 6
+//            4! = 1 x 2 x 3 x 4   = 24
+
+
+//    4. Create an application that simulates dice rolling.
+//
+//    - Ask the user to enter the number of sides for a pair of dice.
+//    - Prompt the user to roll the dice.
+//    - "Roll" two n-sided dice, display the results of each, and then ask the user if he/she wants to roll the dice again.
+//    - Use static methods to implement the method(s) that generate the random numbers.
+//    - Use the .random method of the java.lang.Math class to generate random numbers.
+
+
+
+
+
+
+
+}
