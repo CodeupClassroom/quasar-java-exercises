557aaa405be6a544f715d1f3d6b97e663040e940
Larry Castillo <larrycastillo2009@gmail.com>
2021-11-01 10:24:36 -0500

    complete

--- src/Highlow.java
+++ src/Highlow.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Highlow {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to my number guessing game!");
+        int myNumber = (int)Math.floor(Math.random() * 100);
+        int userInput;
 
+        System.out.println("Guess my number!");
+
+        do{
+            System.out.println("Choose a number between 1-100:");
+            userInput = scanner.nextInt();
+
+            if(userInput > myNumber){
+                System.out.println("LOWER");
+            }else if(userInput < myNumber){
+                System.out.println("HIGHER");
+            }else{
+                System.out.println("GOOD GUESS");
+            }
+        }while(userInput != myNumber);
     }
 
     //    5. Game Development 101
@@ -18,4 +38,7 @@ public class Highlow {
 //    - If user's guess is less than the number, it outputs "HIGHER".
 //    - If user's guess is more than the number, it outputs "LOWER".
 //    - If a user guesses the number, the game should declare "GOOD GUESS!"
+
+
+
 }
--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -1,24 +1,41 @@
-public class MethodsExercise {
-    public static void main(String[] args) {
+import jdk.swing.interop.SwingInterOpUtils;
 
-    }
+import java.util.Scanner;
+
+public class MethodsExercise {
 
 //    1.Basic Arithmetic
 //
 //    a. Create four separate methods. Each will perform an arithmetic operation:
-//
 //    -Addition
 //    -Subtraction
 //    -Multiplication
 //    -Division
-//
 //    b. Each function needs to take two parameters/arguments so that the operation can be performed.
 //    c. Test your methods and verify the output.
 //    d. Add a modulus method that finds the modulus of two numbers.
 //
 //    Food for thought: What happens if we try to divide by zero? What should happen?
 
+    public static int add(int num1, int num2){
+        return num1 + num2;
+    }
 
+    public static int subtract(int num1, int num2){
+        return num1 - num2;
+    }
+
+    public static int multiply(int num1, int num2){
+        return num1 * num2;
+    }
+
+    public static int divide(int num1, int num2){
+        return num1 / num2;
+    }
+
+    public static int modulus(int num1, int num2){
+        return num1 % num2;
+    }
 
 
 
@@ -34,6 +51,17 @@ public class MethodsExercise {
 //      int userInput = getInteger(1, 10);
 
 //    If the input is invalid, prompt the user again.
+
+    public static int getInteger(int min, int max){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number between 1 and 10: ");
+        int userNum = sc.nextInt();
+        if(userNum >= min && userNum <= max){
+            return userNum;
+        }
+        System.out.println("Number not between 1-10. Enter another number:");
+        return getInteger(min, max);
+    }
 
 
 
@@ -55,6 +83,29 @@ public class MethodsExercise {
 //            3! = 1 x 2 x 3       = 6
 //            4! = 1 x 2 x 3 x 4   = 24
 
+    public static void factorial(){
+        Scanner scanner = new Scanner(System.in);
+        int userInput;
+        String isContinue;
+        long factorial = 1;
+        String middle = "1";
+
+        do{
+            userInput = getInteger(1,10);
+            for(int i = 1; i<= userInput; i++){
+                if(i>1){
+                    middle += "x" + i;
+                }
+                factorial *= i;
+                System.out.printf("%2d! = %-45s = %d%n", i,middle,factorial);
+            }
+            System.out.println("Would you like continue? (y/n)");
+             isContinue = scanner.nextLine();
+
+        }while (isContinue.equalsIgnoreCase("y"));
+
+    }
+
 
 //    4. Create an application that simulates dice rolling.
 //
@@ -64,10 +115,45 @@ public class MethodsExercise {
 //    - Use static methods to implement the method(s) that generate the random numbers.
 //    - Use the .random method of the java.lang.Math class to generate random numbers.
 
+    public static void dice(){
+        Scanner sc = new Scanner(System.in);
+        String answer;
+        System.out.println("How many sides would you like this dice to have:");
+        int sides = sc.nextInt();
+        System.out.println("Would you like to roll the dice?(y/n)");
+        String choice = sc.next();
+        if (choice.equalsIgnoreCase("y")){
+            do{
+
+
+            int dice1 = (int) Math.floor(Math.random() * sides);
+            int dice2 = (int) Math.floor(Math.random() * sides);
+            System.out.println(dice1);
+            System.out.println(dice2);
+
+                System.out.println("would to play again?(y/n)");
+                answer = sc.next();
+            }while (answer.equalsIgnoreCase("y"));
+
+        }
+    }
 
 
 
-
+    public static void main(String[] args) {
+//        1
+//        System.out.println("Add: " + add(1, 1));
+//        System.out.println("Subtract: " + subtract(5, 4));
+//        System.out.println("Multiply: " + multiply(6, 4));
+//        System.out.println("Divide: " + divide(8, 2));
+//        System.out.println("Modulus:" + modulus(8, 5));
+//        2
+//        System.out.println(getInteger(1,10));
+//        3
+//        factorial();
+//        4
+        dice();
+    }
 
 
 }
