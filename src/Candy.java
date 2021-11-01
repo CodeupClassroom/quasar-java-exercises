bb6ce1da11b418a07dda1516aaec1b9404f06906
kennethhowell <howell.kenneth.a@gmail.com>
2021-11-01 09:52:59 -0500

    change: small restructure

--- src/Candy.java
+++ src/Candy.java
@@ -6,9 +6,9 @@ public class Candy {
             System.out.printf("Ahh, a %s! I see you also got the %s size of it. Looks good! :) %n", this.name, this.size);
         }
 
-//    public String talkAboutCandy(){
-//        return "Oh, a " + this.name + "! I see you got the " + this.size + " of it";
-//    }
+    public String stringReturnCandy(){
+        return "Oh, a " + this.name + "! I see you got the " + this.size + " of it";
+    }
 
         public Candy(String name){
             this.name = name;
@@ -28,12 +28,14 @@ public class Candy {
             System.out.println("unknownCandy.name = " + unknownCandy.name);
             System.out.println("unknownCandy.size = " + unknownCandy.size);
             unknownCandy.talkaboutCandy();
+            System.out.println(unknownCandy.stringReturnCandy());
 
             Candy funsizeSnickers = new Candy("Snickers", "fun-size");
 
             System.out.println("funsizeSnickers.name = " + funsizeSnickers.name);
             System.out.println("funsizeSnickers.size = " + funsizeSnickers.size);
             funsizeSnickers.talkaboutCandy();
+            System.out.println(funsizeSnickers.stringReturnCandy());
 
 
         }
